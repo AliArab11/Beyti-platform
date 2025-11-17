ad99762dbd12d83a95244ed7c76102d5fd820fb6
Ali Ebrahim Mohamed Ameen Ebrahim Alarab <202200070@student.polytechnic.bh>
2025-11-17 23:08:44 +0300

    feat(structure): add base controllers for all project modules
    Added empty MVC controllers to establish the basic module structure.
    
    Included controllers:
    - Registration
    - Membership
    - Seller
    - Customer
    - Driver
    - Admin
    - ServiceProvider
    - Notification
    
    Each controller contains an Index action returning a View().
    This prepares the project for UI work by all team members.

---
+++ Beyti-MVC/Controllers/DriverController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Beyti_MVC.Controllers
+{
+    public class DriverController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
