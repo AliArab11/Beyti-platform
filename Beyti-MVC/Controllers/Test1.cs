3888f874257b9552a078218c55d420c997455f69
sskob <hussain.shukri13@gmail.com>
2025-11-17 21:38:32 +0300

    test 3

--- Beyti-MVC/Controllers/Test1.cs
+++ Beyti-MVC/Controllers/Test1.cs
@@ -5,7 +5,7 @@ namespace Beyti_MVC.Controllers
     public class Test1 : Controller
     {
         public IActionResult Index()
-
+        // first test commit.
         //this is a one line commit for testing purposes only.
         //Last test.
         {
