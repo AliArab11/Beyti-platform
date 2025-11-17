9666b99cd88bac33225646ffe89c3905d81358f1
Ali Ebrahim Mohamed Ameen Ebrahim Alarab <202200070@student.polytechnic.bh>
2025-11-17 21:12:20 +0300

    test commit

--- Beyti-MVC/Controllers/Test1.cs
+++ Beyti-MVC/Controllers/Test1.cs
@@ -5,6 +5,8 @@ namespace Beyti_MVC.Controllers
     public class Test1 : Controller
     {
         public IActionResult Index()
+
+        //this is a one line commit for testing purposes only.
         {
             return View();
         }
