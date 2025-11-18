58547092444b2ded545892a112fe8bb4ca92da92
Ali Ebrahim Mohamed Ameen Ebrahim Alarab <202200070@student.polytechnic.bh>
2025-11-18 18:00:03 +0300

    Minor edits

--- Beyti-MVC/Program.cs
+++ Beyti-MVC/Program.cs
@@ -8,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<BeytiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BeytiConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,8 +21,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-builder.Services.AddDbContext<BeytiContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("BeytiConnection")));
+
 
 
 app.UseHttpsRedirection();
